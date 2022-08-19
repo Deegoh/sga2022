a0027b09f30296802a614ff7f7eea862d7c50475
Deegoh <tiagopintomonteiro@gmail.com>
2022-08-19 16:48:50 +0200

    add credit

---
+++ Assets/Scripts/CreditMenu.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CreditMenu : MonoBehaviour
+{
+    
+	[Header("PauseMenu")]
+	[SerializeField] private Button _returnButton;
+	[SerializeField] private TextMeshProUGUI _return;
+	private GameObject _lastselect;
+
+	void Start()
+	{
+		_returnButton.onClick.AddListener(ReturnGame);
+	}
+
+	private void Update()
+	{
+		if (!EventSystem.current.currentSelectedGameObject)
+		{
+			EventSystem.current.SetSelectedGameObject(_lastselect);
+		}
+		else
+		{
+			_lastselect = EventSystem.current.currentSelectedGameObject;
+		}
+		_return.color = new Color(0, 242, 255);
+		switch (_lastselect.name)
+		{
+			case "Retour":
+				_return.color = new Color(255, 215, 0);
+				break;
+		}
+	}
+
+	private void OnDestroy()
+	{
+		_returnButton.onClick.RemoveListener(ReturnGame);
+	}
+
+	void ReturnGame()
+	{
+		GameManager.instance.ReturnMainMenu();
+	}
+}
